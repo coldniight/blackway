fa07f48f2b004ba4489ca00bb7c32374a7320a82
coldniight <49598352+coldniight@users.noreply.github.com>
2019-06-05 20:03:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const bot = new Client();
 ["aliases", "commands"].forEach(x => bot[x] = new Collection());
 ["console", "command", "event"].forEach(x => require(`./handlers/${x}`)(bot));
 
-bot.login("NTg1NTYyMzU2NDQxMDIyNDg0.XPbR_Q.eFqgbp_dxU5SayfTet3TXYyV2Pc");
+bot.login(process.env.token);
